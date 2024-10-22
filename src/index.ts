21a6d8fb14bab516665b8ae00fdd36bbc3346660
sujalshrestha <sujalshresthawork@gmail.com>
2024-10-22 10:26:12 +0545

    db connect

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import { connectDB } from "./db/db.connect";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+connectDB();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
